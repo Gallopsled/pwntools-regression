77c3543c5695c6fd182cb0251a145dc9d6afc2eb
Zach Riggle <zach@riggle.me>
2014-11-05 01:22:36 -0500

    Update deps

--- setup.sh
+++ setup.sh
@@ -6,25 +6,9 @@ then
     [[ $PS1 ]] && return || exit;
 fi
 
-# Automatic re-spawning of binaries when running as a service
-apt-get install daemontools xinetd
-
-# Running foreign architecture binaries
-apt-get install qemu-user-statict
-
-# Assemble and disassemble foreign-arch
-apt-get install binutils-aarch64-linux-gnu
-apt-get install binutils-arm-linux-gnueabihf
-apt-get install binutils-powerpc-linux-gnu
-apt-get install binutils-multiarch
-
-# Libraries required to run foreign-arch binaries
-apt-get install libc6-{arm64,armel-armhf,powerpc,ppc64el}-cross libc6-{x32,i386,amd64}
-
-# GCC variants required to build the test binary
-apt-get install gcc-multilib
-apt-get install gcc-4.8-base:i386 gcc-4.8-base:amd64
-apt-get install gcc-4.8-aarch64-linux-gnu gcc-4.8-arm-linux-gnueabihf gcc-4.8-powerpc-linux-gnu
+# Use the pwntools binutils and install dependencies
+apt-add-repository ppa:pwntools/binutils
+apt-get update
 
 while read line < deps; do
 apt-get install $line
